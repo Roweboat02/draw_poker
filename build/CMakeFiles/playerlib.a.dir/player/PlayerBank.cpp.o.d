2907bb475dd1daf27300fae1d9b0f1d61decdbe2
nrrowe <Nrrowe@mun.ca>
2022-12-20 18:13:58 -0330

    exclude build dir

--- build/CMakeFiles/playerlib.a.dir/player/PlayerBank.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/playerlib.a.dir/player/PlayerBank.cpp.o: \
- /home/noahr/CLionProjects/poker/player/PlayerBank.cpp \
- /usr/include/stdc-predef.h \
- /home/noahr/CLionProjects/poker/player/PlayerBank.h
